8f91b7987fe9840d1da4dfc448baef065ea0718c
Gaurav Panchal <gkp.125@gmail.com>
2024-12-25 16:27:34 +0530

    Basic Design Structure Done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,53 +1,81 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={{
+            uri: "https://static.vecteezy.com/system/resources/thumbnails/021/633/453/small/ai-generated-golden-retriever-dog-on-blue-background-photo.jpg",
+          }}
           style={styles.reactLogo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">Itachi!</ThemedText>
+        <ThemedText type="subtitle">French Bulldog . 1y 4m</ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+      <ThemedView>
+        <ThemedView
+          style={{
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            gap: 8,
+          }}
+        >
+          <FontAwesome name="paw" size={24} color="black" />
+          <ThemedText type="title">About Itachi</ThemedText>
+        </ThemedView>
+
+        <ThemedView style={{ marginTop: 16, width: "100%" }}>
+          <ThemedView style={{ flexDirection: "row", gap: 16 }}>
+            <ThemedView
+              style={{
+                padding: 16,
+                backgroundColor: "#F0F0F0",
+                borderRadius: 8,
+              }}
+            >
+              <ThemedText type="subtitle">Breed</ThemedText>
+              <ThemedText type="subtitle">French Bulldog</ThemedText>
+            </ThemedView>
+
+            <ThemedView
+              style={{
+                padding: 16,
+                backgroundColor: "#F0F0F0",
+                borderRadius: 8,
+              }}
+            >
+              <ThemedText type="subtitle">Breed</ThemedText>
+              <ThemedText type="subtitle">French Bulldog</ThemedText>
+            </ThemedView>
+
+            <ThemedView
+              style={{
+                padding: 16,
+                backgroundColor: "#F0F0F0",
+                borderRadius: 8,
+              }}
+            >
+              <ThemedText type="subtitle">Breed</ThemedText>
+              <ThemedText type="subtitle">French Bulldog</ThemedText>
+            </ThemedView>
+          </ThemedView>
+        </ThemedView>
+
+        <ThemedText type="subtitle" style={{ marginTop: 16 }}>
+          Itachi is a very playful and energetic dog. He loves to play fetch and
+          run around the park. He is very friendly and loves to meet new people
+          and dogs.
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
@@ -56,19 +84,14 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+    display: "flex",
+    flexDirection: "column",
   },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
